6e1ee5370b1a645640678f84cb80152e22242fe5
Chitranshi1~ <chitranshigourana@gmail.com>
2024-01-17 18:00:06 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import Navbar from './Components/navbar'
+import Navbar from './Components/Navbar'
 import HeroSection from './Components/HeroSection'
 
 const App = () => {
